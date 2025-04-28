3c938bcd5a766b44378651b22c90bd16dc7e1e69
Michael Bahr <michael.bahr@sourcegraph.com>
2025-04-28 16:16:52 +0200

    shorten files

--- generate_folders.sh
+++ generate_folders.sh
@@ -16,7 +16,7 @@ for ((i=1; i<=N; i++)); do
   mkdir -p "$FOLDER_NAME/subfolder"
 
 # Lorem ipsum text to use
-LOREM_IPSUM="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+LOREM_IPSUM="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
 
   # Create README.md with i lines of lorem ipsum
   for ((j=1; j<=i; j++)); do
